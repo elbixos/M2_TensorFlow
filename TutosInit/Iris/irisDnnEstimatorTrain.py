68e1e49d02e90ae578523b6746bea24a676994d6
Vincent Page <vincent.page@univ-antilles.fr>
2018-02-05 10:18:39 -0400

    Nettoyage Iris
    presque propre

--- TutosInit/Iris/irisDnnEstimatorTrain.py
+++ TutosInit/Iris/irisDnnEstimatorTrain.py
@@ -43,7 +43,7 @@ feature_columns = [tf.feature_column.numeric_column("x", shape=[4])]
 classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
                                       hidden_units=[10, 20, 10],
                                       n_classes=3,
-                                      model_dir="./Iris_model")
+                                      model_dir="./VisuDnn")
 # Define the training inputs
 train_input_fn = tf.estimator.inputs.numpy_input_fn(
   x={"x": np.array(training_set.data)},
@@ -98,20 +98,3 @@ accuracy_score = classifier.evaluate(input_fn=test_input_fn)["accuracy"]
 print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
 
 
-
-# Classify two new flower samples.
-new_samples = np.array(
-  [[6.9, 3.2, 4.5, 1.5],
-   [4.8, 3.1, 5.0, 1.7]], dtype=np.float32)
-predict_input_fn = tf.estimator.inputs.numpy_input_fn(
-  x={"x": new_samples},
-  num_epochs=1,
-  shuffle=False)
-
-dicoClasses = ['setosa', 'versicolor', 'virginica']
-predictions = classifier.predict(input_fn=predict_input_fn)
-
-for p in predictions :
-    class_id = p['class_ids'][0]
-    probability = p['probabilities'][class_id]
-    print ("je pense que c'est : ",dicoClasses[class_id], "avec une proba de ",probability )   
